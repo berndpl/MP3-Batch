0b2fe4fcff8135d2cc4545666b7e517227aa8613
Bernd Plontsch <bernd.plontsch@gmail.com>
2010-10-31 16:28:01 +0100

    Wording ...

--- mp3batch.rb
+++ mp3batch.rb
@@ -1,26 +1,24 @@
 #!/usr/bin/ruby
-
 require "FileUtils"
 
+#Setup variables
 targetdir = 'Done'
 
-puts "-- Converting all WAV files in this folder to MP3s --"
-
+#Go!
+puts "-- Converting all WAV files in this folder to MP3s. When done the WAV can be found in the /done folder --"
 source_files = Dir.glob('*.wav') 
-
-#check for destination folders/create them
 Dir.mkdir("#{targetdir}") unless File.directory?("#{targetdir}")
 
 source_files.each do |file|
   filebase = file.delete ".wav"
-  splitted = filebase.split("_-_").map {|s| s.gsub("_"," ") }
+  splitted = filebase.split("_-_").collect {|s| s.gsub("_"," ") }
   artisttag = splitted[0]
   titletag = splitted[1]    
   lame = "lame -V2 \"#{file}\" \"#{filebase}.mp3\" --ty \"#{Time.now.year}\" --tt \"#{titletag}\" --ta \"#{artisttag}\""
   puts lame
   %x[#{lame}]
   if File.exists?("#{filebase}.mp3") 
-    puts ("final file found. cool.")
+    puts ("Encoded file found. Cool.")
     FileUtils.move("#{file}","#{targetdir}")
   end
 end
