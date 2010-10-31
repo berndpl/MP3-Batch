9d7dd1ccaacda75cc2063a14de6dd366dab932da
Bernd Plontsch <bernd.plontsch@gmail.com>
2010-10-31 18:13:32 +0100

    introducing convert function, path tutorial

--- mp3batch.rb
+++ mp3batch.rb
@@ -1,25 +1,42 @@
 #!/usr/bin/ruby
 require "FileUtils"
 
-#Setup variables
-targetdir = 'Done'
+#----------------------------------------
+# Setup variables
+#----------------------------------------
 
-#Go!
-puts "-- Converting all WAV files in this folder to MP3s. When done the WAV can be found in the /done folder --"
-source_files = Dir.glob('*.wav') 
-Dir.mkdir("#{targetdir}") unless File.directory?("#{targetdir}")
+targetdir = 'Originals'
 
-source_files.each do |file|
-  filebase = file.delete ".wav"
-  splitted = filebase.split("_-_").collect {|s| s.gsub("_"," ") }
-  artisttag = splitted[0]
-  titletag = splitted[1]    
-  lame = "lame -V2 \"#{file}\" \"#{filebase}.mp3\" --ty \"#{Time.now.year}\" --tt \"#{titletag}\" --ta \"#{artisttag}\""
-  puts lame
-  %x[#{lame}]
-  if File.exists?("#{filebase}.mp3") 
-    puts ("Encoded file found. Cool.")
-    FileUtils.move("#{file}","#{targetdir}")
+#----------------------------------------
+# Methods
+#----------------------------------------
+
+def convert(source_files,targetdir)
+  Dir.mkdir("#{targetdir}") unless File.directory?("#{targetdir}")
+ 
+  source_files.each do |file|
+    filebase = file.delete ".wav"
+    splitted = filebase.split("_-_").collect {|s| s.gsub("_"," ") }
+    artisttag = splitted[0]
+    titletag = splitted[1]    
+    lame = "lame -V2 \"#{file}\" \"#{filebase}.mp3\" --ty \"#{Time.now.year}\" --tt \"#{titletag}\" --ta \"#{artisttag}\""
+    puts lame
+    %x[#{lame}]
+    if File.exists?("#{filebase}.mp3") 
+      puts ("\nEncoded file found. Cool.\n\n")
+      FileUtils.move("#{file}","#{targetdir}")
+    end
   end
 end
-  
+
+#----------------------------------------
+# Process
+#----------------------------------------
+
+source_files = Dir.glob('*.wav')
+if source_files.empty? then
+    puts "\nNo .WAV found in this folder.\n\n"
+else
+    puts "\nConverting all WAV files in this folder to MP3s. When done the WAV can be found in the /#{targetdir} folder\n\n"
+    convert(source_files,targetdir)
+end
